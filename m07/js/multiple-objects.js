131cf54ae957803ef91b91c32c6fbee01d455b10
ella11-a <133909158+ella11-a@users.noreply.github.com>
2024-05-18 14:45:57 -0700

    Update multiple-objects.js

--- m07/js/multiple-objects.js
+++ m07/js/multiple-objects.js
@@ -26,7 +26,7 @@ var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
 var details3 = sunsetHotel.name + ' rooms: ';
-    detals3 += sunsetHotel.checkAvailability();
+    details3 += sunsetHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details3;
 
